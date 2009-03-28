450e0d5947276ba176a425148b8a6594a567bbd2
Kevin <klcompt@gmail.com>
2009-03-27 21:38:10 -0500

    fixing controller by commenting out new method

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -5,9 +5,9 @@ class Admin::LinksController < Admin::ResourceController
     @links = Link.find(:all, :order => 'created_at DESC')
   end
 
-  #allow title and url to be set with a bookmarklet
-  def new    
-    self.model = model_class.new(:title => params[:title], :url => params[:url])
-    render :template => "admin/links/edit" if handle_new_or_edit_post
-  end  
+#  #allow title and url to be set with a bookmarklet
+#  def new    
+#    self.model = model_class.new(:title => params[:title], :url => params[:url])
+#    render :template => "admin/links/edit" if handle_new_or_edit_post
+#  end  
 end
