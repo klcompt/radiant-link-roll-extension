fe30d628e3a8a2fe63cc81b0099604e2fbffc137
Kevin <klcompt@gmail.com>
2009-03-27 21:56:23 -0500

    ..

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -6,7 +6,7 @@ class Admin::LinksController < Admin::ResourceController
   end
 # 
 #  #allow title and url to be set with a bookmarklet
-#  def new    
+##  def new    
 #    self.model = model_class.new(:title => params[:title], :url => params[:url])
 #    render :template => "admin/links/edit" if handle_new_or_edit_post
 #  end  
